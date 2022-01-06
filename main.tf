2f5482898a19f668fc83f67210efcecc3d1c3938
jo763 <joe.prollins@otmail.com>
2022-01-06 15:35:39 +0000

    added vpc and instance TF code

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# eng99_terraform
-- Setting up a terraform - simple EC2 instance using an ami
-```
 # Let Terraform know who is our cloud provider
 
 # AWS plugins/dependencies will be downloaded
@@ -10,9 +7,8 @@ provider "aws"{
 
 }
 # Let's start with launching an EC2 instance using TF
-
 resource "aws_instance" "app_instance" {
-  ami = "ami-07d8796a2b0f8d29c"
+  ami = var.app_ami_id
   instance_type = "t2.micro"
   # Enable public IP
   associate_public_ip_address = true
@@ -21,14 +17,14 @@ resource "aws_instance" "app_instance" {
   }
   # added eng99.pem so one can ssh
   # Ensure we have this key in the .ssh folder
-  key_name = "eng99"
+  key_name = var.aws_key_name
 }
 
-module "vpc" {
+module "aws_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "eng99_joseph_terraform_VPC"
-  cidr = "10.0.0.0/16"
+  cidr = var.cidr_block
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -43,19 +39,7 @@ module "vpc" {
     Environment = "dev"
   }
 }
-
-resource "aws_vpc" "eng99_joseph_terraform_VPC" {
-  cidr = "10.0.0.0/16"
-
-  tags = {
-    Name = "10.0.0.0/18"
-  }
-}
-
-
-
 # To intialise we use terraform init
 # terraform plan
 # terraform apply
 # terraform destroy
-```
