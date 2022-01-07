6c4a8057765a2343a134613dc8b292663ddf5e0d
jo763 <joe.prollins@otmail.com>
2022-01-07 17:44:06 +0000

    opened SG to world

--- main.tf
+++ main.tf
@@ -121,10 +121,19 @@ resource "aws_security_group" "allow_tls" {
   }
 
   ingress {
-    description      = "ssh from world"
+    description      = "ssh public access UNSECURE"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "ENTER THROUGH ANYWHERE VERY UNSECURE!!!!!"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
